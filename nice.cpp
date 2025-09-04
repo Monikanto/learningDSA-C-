143fa049a45e4583e0c4ae19e65f490c793047ab
Monikanto <manikurmi14@gmail.com>
2025-09-04 23:11:40 +0530

    first commit

---
+++ nice.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std ;
+ 
+int main(){
+    string hey = "hey man how you guys are doing";
+    cout <<"hope this time it works";
+    cout << "hello , yeah im testing";
+    int len = hey.size();
+
+    cout << len;
+}
